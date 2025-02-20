785c3049d0b1835b653d63422246325fad0cd1dc
candeodevelopment <development@candeo.io>
2025-02-20 16:23:55 +0000

    Update Candeo C205 Zigbee Switch Module (With Or Without Neutral).groovy
    Adds setting for:
    
    1) Switch input type (momentary or toggle)

--- Candeo C205 Zigbee Switch Module (With Or Without Neutral).groovy
+++ Candeo C205 Zigbee Switch Module (With Or Without Neutral).groovy
@@ -1,11 +1,12 @@
 /**
  *    Candeo C205 Zigbee Switch Module (With Or Without Neutral)
- *    Supports Toggle Switches
+ *    Supports Momentary & Toggle Switches
  *    Supports on / off / flash
  *    Reports switch events
  *    Has Setting For Flash Time
  *    Has Setting For Flash Timeout
  *    Has Setting For Power On Default
+ *    Has Setting For Switch Input Type
  *    Has Setting For Explicit State After Hub Startup
  */
 
@@ -18,6 +19,7 @@ metadata {
         capability 'Refresh'
         capability 'Configuration'
 
+        command 'resetPreferencesToDefault'
         fingerprint profileId: '0104', endpointId: '01', inClusters: '0000,0003,0004,0005,0006,0B05,1000', outClusters: '0019', manufacturer: 'Candeo', model: 'C205', deviceJoinName: 'Candeo C205 Zigbee Switch Module (With Or Without Neutral)'
     }
     preferences {
@@ -27,7 +29,8 @@ metadata {
         input name: 'hubStartupDefaultCommand', type: 'enum', title: 'Explicit Command After Hub Has Restarted', description: '<small>After the hub restarts, carry out this command on the device.</small><br><br>', options: PREFHUBRESTART, defaultValue: 'refresh'
         input name: 'loggingOption', type: 'enum', title: 'Logging Option', description: '<small>Sets the logging level cumulatively, for example "Driver Trace Logging" will include all logging levels below it.</small><br><br>', options: PREFLOGGING, defaultValue: '5'
         input name: 'deviceConfigurationOptions', type: 'hidden', title: '<strong>Device Configuration Options</strong>', description: '<small>The following options change the behaviour of the device itself, they take effect after hitting "<strong>Save Preferences</strong> below", followed by "<strong>Configure</strong>" above.<br><br>For a battery powered device, you may also need to wake it up manually!</small>'
-        input name: 'powerOnDefault', type: 'enum', title: 'Default State After Return From Power Failure', description: '<small>After a power failure, set the device to this state when the power is restored.</small><br><br>', options: PREFPOWERON, defaultValue: 'previous'
+        input name: 'deviceConfigDefaultPowerOnBehaviour', type: 'enum', title: 'Default State After Return From Power Failure', description: '<small>After a power failure, set the device to this state when the power is restored.</small><br><br>', options: PREFPOWERON, defaultValue: 'previous'
+        input name: 'deviceConfigSwitchInputType', type: 'enum', title: 'Switch Input Type', description: '<small>Select the type of switch connected to the input. <strong>Power cycle the module for the setting to take effect.</strong></small><br><br>', options: PREFSWITCHINPUTTYPE, defaultValue: 'toggle'
         input name: 'platformOptions', type: 'hidden', title: '<strong>Platform Options</strong>', description: '<small>The following options are relevant to the Hubitat platform and UI itself.</small>'
     }
 }
@@ -37,24 +40,39 @@ import groovy.transform.Field
 private @Field final String CANDEO = 'Candeo C205 Device Driver'
 private @Field final Boolean DEBUG = false
 private @Field final Integer LOGSOFF = 1800
+private @Field final Integer ZIGBEEDELAY = 1000
 private @Field final Map PREFFALSE = [value: 'false', type: 'bool']
 private @Field final Map PREFTRUE = [value: 'true', type: 'bool']
+private @Field final Map PREFPREVIOUS = [value: 'previous', type: 'enum']
+private @Field final Map PREF10 = [value: '10', type: 'enum']
+private @Field final Map PREF5 = [value: '5', type: 'enum']
 private @Field final Map PREFPOWERON = [ 'off': 'Off', 'on': 'On', 'opposite': 'Opposite', 'previous': 'Previous' ]
+private @Field final Map PREFSWITCHINPUTTYPE = [ 'momentary': 'Momentary', 'toggle': 'Toggle' ]
 private @Field final Map PREFHUBRESTART = [ 'off': 'Off', 'on': 'On', 'refresh': 'Refresh State Only', 'nothing': 'Do Nothing' ]
 private @Field final Map PREFFLASHTIME = ['500': '500ms', '750': '750ms', '1000': '1000ms', '1500': '1500ms', '2000': '2000ms', '2500': '2500ms', '3000': '3000ms', '4000': '4000ms', '5000': '5000ms']
 private @Field final Map PREFFLASHTIMEOUT = ['0': 'never', '1': '1m', '2': '2m', '3': '3m', '4': '4m', '5': '5m', '10': '10m', '15': '15m', '30': '30m', '60': '60m', '90': '90m', '120': '120m', '180': '180m']
 private @Field final Map PREFLOGGING = ['0': 'Device Event Logging', '1': 'Driver Informational Logging', '2': 'Driver Warning Logging', '3': 'Driver Error Logging', '4': 'Driver Debug Logging', '5': 'Driver Trace Logging' ]
 
 void installed() {
+    logTrace('installed called', true)
+    resetPreferencesToDefault()
+}
+
+void resetPreferencesToDefault() {
     logsOn()
-    logTrace('installed called')
-    device.updateSetting('powerOnDefault', [value: 'previous', type: 'enum'])
-    logInfo("powerOnDefault setting is: ${PREFPOWERON[powerOnDefault]}")
+    logTrace('resetPreferencesToDefault called')
+    settings.keySet().each { String setting ->
+        device.removeSetting(setting)
+    }
+    device.updateSetting('deviceConfigDefaultPowerOnBehaviour', PREFPREVIOUS)
+    logInfo("deviceConfigDefaultPowerOnBehaviour setting is: ${PREFPOWERON[deviceConfigDefaultPowerOnBehaviour]}")
+    device.updateSetting('deviceConfigSwitchInputType', [value: 'toggle', type: 'enum'])
+    logInfo("deviceConfigSwitchInputType setting is: ${PREFSWITCHINPUTTYPE[deviceConfigSwitchInputType]}")
     device.updateSetting('hubStartupDefaultCommand', [value: 'refresh', type: 'enum'])
     logInfo("hubStartupDefaultCommand setting is: ${PREFHUBRESTART[hubStartupDefaultCommand]}")
     device.updateSetting('flashTime', [value: '750', type: 'enum'])
     logInfo("flashTime setting is: ${PREFFLASHTIME[flashTime]}")
-    device.updateSetting('flashTimeout', [value: '10', type: 'enum'])
+    device.updateSetting('flashTimeout', PREF10)
     logInfo("flashTimeout setting is: ${PREFFLASHTIMEOUT[flashTimeout]}")
     logInfo('logging level is: Driver Trace Logging')
     logInfo("logging level will reduce to Driver Error Logging after ${LOGSOFF} seconds")
@@ -83,10 +101,11 @@ void initialize() {
     }
 }
 
-void updated() {
+List<String> updated() {
     logTrace('updated called')
     logTrace("settings: ${settings}")
-    logInfo("powerOnDefault setting is: ${PREFPOWERON[powerOnDefault ?: 'previous']}", true)
+    logInfo("deviceConfigDefaultPowerOnBehaviour setting is: ${PREFPOWERON[deviceConfigDefaultPowerOnBehaviour ?: 'previous']}", true)
+    logInfo("deviceConfigSwitchInputType setting is: ${PREFSWITCHINPUTTYPE[deviceConfigSwitchInputType ?: 'toggle']}", true)
     logInfo("hubStartupDefaultCommand setting is: ${PREFHUBRESTART[hubStartupDefaultCommand ?: 'refresh']}", true)
     logInfo("flashTime setting is: ${PREFFLASHTIME[flashTime ?: '750']}", true)
     logInfo("flashTimeout setting is: ${PREFFLASHTIMEOUT[flashTimeout ?: '10']}", true)
@@ -96,7 +115,10 @@ void updated() {
         logInfo("logging level will reduce to Driver Error Logging after ${LOGSOFF} seconds", true)
         runIn(LOGSOFF, logsOff)
     }
-    logInfo('if you have changed any Device Configuration Options, make sure that you hit Configure above!', true)
+    if (checkPreferences()) {
+        logInfo('Device Configuration Options have been changed, will now configure the device!', true)
+        return configure()
+    }
 }
 
 void logsOff() {
@@ -112,19 +134,32 @@ void logsOff() {
 
 List<String> configure() {
     logTrace('configure called')
+    logDebug("startup on off is ${deviceConfigDefaultPowerOnBehaviour ?: 'previous'}")
     Map<String, String> startUpOnOff = ['on': 0x01, 'off': 0x0, 'opposite': 0x02, 'previous': 0x03]
-    List<String> cmds = ["zdo bind 0x${device.deviceNetworkId} 0x${device.endpointId} 0x01 0x0006 {${device.zigbeeId}} {}", 'delay 2000',
-                         "he cr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x0000 ${DataType.BOOLEAN} 0 3600 {}", 'delay 2000',
-                         "he wattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x4003 0x30 {${startUpOnOff[powerOnDefault ?: 'previous']}} {}", 'delay 2000',
-                         "he rattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x4003 {}", 'delay 2000']
+    logDebug("startUpOnOff: ${startUpOnOff[deviceConfigDefaultPowerOnBehaviour ?: 'previous']}")
+    logDebug("switch input type is ${deviceConfigSwitchInputType ?: 'toggle'}")
+    Map<String, String> deviceConfigSwitchInputTypes = ['momentary': 0x00, 'toggle': 0x01]
+    logDebug("switchInputType: ${deviceConfigSwitchInputTypes[deviceConfigSwitchInputType ?: 'toggle']}")
+    List<String> cmds = [//onoff
+                         "zdo bind 0x${device.deviceNetworkId} 0x${device.endpointId} 0x01 0x0006 {${device.zigbeeId}} {}", "delay ${ZIGBEEDELAY}",
+                         "he cr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x0000 ${DataType.BOOLEAN} 0 3600 {}", "delay ${ZIGBEEDELAY}",
+                         "he raw 0x${device.deviceNetworkId} 1 0x${device.endpointId} 0x0006 {10 00 08 00 00 00}", "delay ${ZIGBEEDELAY}",
+                         "he rattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x0000 {}", "delay ${ZIGBEEDELAY}",
+                         //startupbehaviour
+                         "he rattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x4003 {}", "delay ${ZIGBEEDELAY}",
+                         "he wattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x4003 ${convertToHexString(DataType.ENUM8)} {${startUpOnOff[deviceConfigDefaultPowerOnBehaviour ?: 'previous']}} {}", "delay ${ZIGBEEDELAY}",
+                         "he rattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x4003 {}", "delay ${ZIGBEEDELAY}",
+                         //switchinputtype
+                         "he raw 0x${device.deviceNetworkId} 0x01 0x${device.endpointId} 0x0000 {04 24 12 00 00 03 88}", "delay ${ZIGBEEDELAY}",
+                         "he wattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0000 0x8803 ${convertToHexString(DataType.UINT8)} {${deviceConfigSwitchInputTypes[deviceConfigSwitchInputType ?: 'toggle']}} {1224}", "delay ${ZIGBEEDELAY}",
+                         "he raw 0x${device.deviceNetworkId} 0x01 0x${device.endpointId} 0x0000 {04 24 12 00 00 03 88}", "delay ${ZIGBEEDELAY}"]
     logDebug("sending ${cmds}")
     return cmds
 }
 
 List<String> refresh() {
     logTrace('refresh called')
-    List<String> cmds = ["he rattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x0000 {}", 'delay 2000',
-                         "he rattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x4003 {}", 'delay 2000']
+    List<String> cmds = ["he rattr 0x${device.deviceNetworkId} 0x${device.endpointId} 0x0006 0x0000 {}", "delay ${ZIGBEEDELAY}"]
     logDebug("sending ${cmds}")
     return cmds
 }
@@ -166,9 +201,9 @@ void flash(BigDecimal rate = null) {
             flashRate = flashTime ? flashTime.toInteger() : 750
         }
         logDebug("flashRate: ${flashRate}")
-        if (flashRate < 500) {
-            logWarn('flashRate is lower than 500ms, resetting to safe value')
-            flashRate = 500
+        if (flashRate > 5000 || flashRate < 500) {
+            flashRate = flashRate > 5000 ? 5000 : flashRate < 500 ? 500 : flashRate
+            logWarn('flashRate outside safe range (500 - 5000), resetting to safe value!')
         }
         runInMillis(flashRate, flasher, [data: ['on': !(state['flashPrevious']), 'rate': flashRate]])
         logDebug("flashTimeout: ${flashTimeout ?: '10'}")
@@ -266,7 +301,10 @@ private List<Map> processEvents(Map descriptionMap, List<Map> events = []) {
         logTrace('skipping ZDP profile message')
     }
     else if (!(descriptionMap.profileId) || (descriptionMap.profileId && descriptionMap.profileId == '0104')) {
-        if (descriptionMap.cluster == '0006' || descriptionMap.clusterId == '0006' || descriptionMap.clusterInt == 6) {
+        if (descriptionMap.cluster == '0000' || descriptionMap.clusterId == '0000' || descriptionMap.clusterInt == 0) {
+            processBasicEvent(descriptionMap, events)
+        }
+        else if (descriptionMap.cluster == '0006' || descriptionMap.clusterId == '0006' || descriptionMap.clusterInt == 6) {
             processSwitchEvent(descriptionMap, events)
         }
         else {
@@ -284,6 +322,37 @@ private List<Map> processEvents(Map descriptionMap, List<Map> events = []) {
         }
     }
     return events
+}
+
+private void processBasicEvent(Map descriptionMap, List<Map> events) {
+    logTrace('processBasicEvent called')
+    switch (descriptionMap.command) {
+        case '0A':
+        case '01':
+            if (descriptionMap.attrId == '8803' || descriptionMap.attrInt == 34819) {
+                logDebug('basic (0000) switch type report (8803)')
+                String deviceConfigSwitchInputTypeValue = descriptionMap.value
+                Map<String, String> deviceConfigSwitchInputTypes = ['00': 'momentary', '01': 'toggle']
+                String deviceConfigSwitchInputTypeBehaviour = PREFSWITCHINPUTTYPE[deviceConfigSwitchInputTypes[deviceConfigSwitchInputTypeValue]] ?: "${deviceConfigSwitchInputTypeValue} (unknown}"
+                logDebug("deviceConfigSwitchInputType is currently set to: ${PREFSWITCHINPUTTYPE[deviceConfigSwitchInputType ?: 'previous']} and device reports it is set to: ${deviceConfigSwitchInputTypeBehaviour}")
+            }
+            else {
+                logDebug('basic (0000) attribute skipped')
+            }
+            break
+        case '04':
+            logDebug('basic (0000) write attribute response (04) skipped')
+            break
+        case '07':
+            logDebug('basic (0000) configure reporting response (07) skipped')
+            break
+        case '0B':
+            logDebug('basic (0000) default response (0B) skipped')
+            break
+        default:
+            logDebug('basic (0000) command skipped')
+            break
+    }
 }
 
 private void processSwitchEvent(Map descriptionMap, List<Map> events) {
@@ -322,7 +391,7 @@ private void processSwitchEvent(Map descriptionMap, List<Map> events) {
                 String startUpOnOffValue = descriptionMap.value
                 Map<String, String> startUpOnOff = ['00': 'off', '01': 'on', '02': 'opposite', '03': 'previous', 'FF': 'previous']
                 String startupBehaviour = PREFPOWERON[startUpOnOff[startUpOnOffValue]] ?: "${startUpOnOffValue} (unknown}"
-                logDebug("powerOnDefault is currently set to: ${PREFPOWERON[powerOnDefault ?: 'previous']} and device reports it is set to: ${startupBehaviour}")
+                logDebug("deviceConfigDefaultPowerOnBehaviour is currently set to: ${PREFPOWERON[deviceConfigDefaultPowerOnBehaviour ?: 'previous']} and device reports it is set to: ${startupBehaviour}")
             }
             else {
                 logDebug('on off (0006) attribute skipped')
@@ -350,7 +419,7 @@ private Map processEvent(Map event) {
 
 private Boolean logMatch(String logLevel) {
     Map<String, String> logLevels = ['event': '0', 'info': '1', 'warn': '2', 'error': '3', 'debug': '4', 'trace': '5' ]
-    return loggingOption ? loggingOption.toInteger() >= logLevels[logLevel].toInteger() : false
+    return loggingOption ? loggingOption.toInteger() >= logLevels[logLevel].toInteger() : true
 }
 
 private String logTrace(String msg, Boolean override = false) {
@@ -397,7 +466,7 @@ private String logMsg(String msg) {
 
 private void logsOn() {
     logTrace('logsOn called', true)
-    device.updateSetting('loggingOption', [value: '5', type: 'enum'])
+    device.updateSetting('loggingOption', PREF5)
     runIn(LOGSOFF, logsOff)
 }
 
@@ -412,4 +481,48 @@ private void doZigBeeCommand(List<String> cmds) {
     logTrace('doZigBeeCommand called')
     logDebug("sending ${cmds}")
     sendHubCommand(new hubitat.device.HubMultiAction(cmds, hubitat.device.Protocol.ZIGBEE))
+}
+
+private String convertToHexString(String value, Integer minBytes = 1, Boolean reverse = false) {
+    return convertToHexString(convertToInteger(value), minBytes, reverse)
+}
+
+private List<String> convertToHexString(List<?> values, Integer minBytes = 1, Boolean reverse = false) {
+    return values.collect { value -> convertToHexString(value, minBytes, reverse) }
+}
+
+private String convertToHexString(Integer value, Integer minBytes = 1, Boolean reverse = false) {
+    logTrace("convertToHexString called value: ${value} minBytes: ${minBytes} reverse: ${reverse}")
+    String hexString = hubitat.helper.HexUtils.integerToHexString(value, minBytes)
+    if (reverse) {
+        return reverseStringOfBytes(hexString)
+    }
+    return hexString
+}
+
+private String reverseStringOfBytes(String value) {
+    logTrace("reverseStringOfBytes called value: ${value}")
+    return value.split('(?<=\\G..)').reverse().join()
+}
+
+private Boolean checkPreference(String preference) {
+    logTrace("checkPreference called preference: ${preference}")
+    String oldPreference = preference + '_OLD'
+    String newPreferenceValue = settings.containsKey(preference) ? settings[preference].toString() : 'unknown'
+    logDebug("newPreferenceValue: ${newPreferenceValue}")
+    String oldPreferenceValue = settings.containsKey(oldPreference) ? settings[oldPreference].toString() : 'unknown'
+    logDebug("oldPreferenceValue: ${oldPreferenceValue}")
+    if (oldPreferenceValue != newPreferenceValue) {
+        device.updateSetting(oldPreference, newPreferenceValue)
+        return true
+    }
+    return false
+}
+
+private Boolean checkPreferences() {
+    logTrace('checkPreferences called')
+    Set deviceConfig = settings.keySet().findAll { String preference ->
+        preference.startsWith('deviceConfig') && preference.indexOf('_OLD') == -1 && checkPreference(preference)
+    }
+    return (deviceConfig.size() > 0)
 }
