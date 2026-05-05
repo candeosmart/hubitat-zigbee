e1ec7298bc3a10c83f7a1444b46f30df2f3b5381
candeodevelopment <development@candeo.io>
2026-05-05 14:43:21 +0100

    Create Candeo C-ZB-SM30-2G Zigbee 2-Gang Switch Module (L+N).groovy and Candeo C-ZB-SM30-2G Zigbee 2-Gang Switch Module (L+N) Child Switch.groovy
    Adds support for Candeo C-ZB-SM30-2G Zigbee 2-Gang Switch Module (L+N)

---
+++ Candeo C-ZB-SM30-2G Zigbee 2-Gang Switch Module (L+N) Child Switch.groovy
@@ -0,0 +1,405 @@
+/**
+ *    Candeo C-ZB-SM30-2G Zigbee 2-Gang Switch Module (L+N) Child Switch
+ *    Supports Momentary & Toggle Switches
+ *    Supports on / off / flash
+ *    Reports switch events
+ *    Has Setting For Flash Time
+ *    Has Setting For Flash Timeout
+ *    Has Setting For Power On Default
+ *    Has Setting For Automatic Off Time
+ *    Has Setting For Explicit State After Hub Startup
+ */
+
+metadata {
+    definition(name: 'Candeo C-ZB-SM30-2G Zigbee 2-Gang Switch Module (L+N) Child Switch', namespace: 'Candeo', author: 'Candeo', component: true, importUrl: 'TBA', singleThreaded: true) {
+        capability 'Switch'
+        capability 'Flash'
+        capability 'Actuator'
+        capability 'Initialize'
+        capability 'Refresh'
+        capability 'Configuration'
+
+        command 'onWithAutomaticOff', [ [ name:'Automatic Off Time*', type: 'NUMBER', description: 'Automatically turn the output off after this time (ms).' ] ]
+        command 'resetPreferencesToDefault'
+    }
+    preferences {
+        input name: 'deviceDriverOptions', type: 'hidden', title: '<strong>Device Driver Options</strong>', description: '<small>The following options change the behaviour of the device driver, they take effect after hitting "<strong>Save Preferences</strong> below."</small>'
+        input name: 'automaticOffTime', type: 'enum', title: 'Automatic Off Time (s)', description: '<small>When turning the device on, use this as the automatic off time if one is not specified explicitly.</small><br><br>', options: PREFAUTOMATICOFFTIME, defaultValue: 'none'
+        input name: 'flashTime', type: 'enum', title: 'Flash Time (ms)', description: '<small>When carrying out a flash command, use this as the on and off time.</small><br><br>', options: PREFFLASHTIME, defaultValue: '750'
+        input name: 'flashTimeout', type: 'enum', title: 'Flash Timeout (m)', description: '<small>When carrying out a flash command, automatically cancel after this amount of time.</small><br><br>', options: PREFFLASHTIMEOUT, defaultValue: '10'
+        input name: 'hubStartupDefaultCommand', type: 'enum', title: 'Explicit Command After Hub Has Restarted', description: '<small>After the hub restarts, carry out this command on the device.</small><br><br>', options: PREFHUBRESTART, defaultValue: 'refresh'
+        input name: 'loggingOption', type: 'enum', title: 'Logging Option', description: '<small>Sets the logging level cumulatively, for example "Driver Trace Logging" will include all logging levels below it.</small><br><br>', options: PREFLOGGING, defaultValue: '5'
+        input name: 'deviceConfigurationOptions', type: 'hidden', title: '<strong>Device Configuration Options</strong>', description: '<small>The following options change the behaviour of the device itself, they take effect after hitting "<strong>Save Preferences</strong> below", followed by "<strong>Configure</strong>" above.<br><br>For a battery powered device, you may also need to wake it up manually!</small>'
+        input name: 'deviceConfigDefaultPowerOnBehaviour', type: 'enum', title: 'Default State After Return From Power Failure', description: '<small>After a power failure, set the device to this state when the power is restored.</small><br><br>', options: PREFPOWERON, defaultValue: 'previous'
+        input name: 'platformOptions', type: 'hidden', title: '<strong>Platform Options</strong>', description: '<small>The following options are relevant to the Hubitat platform and UI itself.</small>'
+    }
+}
+
+import groovy.transform.Field
+
+private @Field final String CANDEO = 'Candeo C-ZB-SM30-2G Device Driver'
+private @Field final Boolean DEBUG = false
+private @Field final Integer LOGSOFF = 1800
+private @Field final Map PREFFALSE = [value: 'false', type: 'bool']
+private @Field final Map PREFTRUE = [value: 'true', type: 'bool']
+private @Field final Map PREFPREVIOUS = [value: 'previous', type: 'enum']
+private @Field final Map PREFNONE = [value: 'none', type: 'enum']
+private @Field final Map PREF10 = [value: '10', type: 'enum']
+private @Field final Map PREF5 = [value: '5', type: 'enum']
+private @Field final Map PREFPOWERON = [ 'off': 'Off', 'on': 'On', 'previous': 'Previous' ]
+private @Field final Map PREFHUBRESTART = [ 'off': 'Off', 'on': 'On', 'refresh': 'Refresh State Only', 'nothing': 'Do Nothing' ]
+private @Field final Map PREFAUTOMATICOFFTIME = ['none': 'do not automatically turn off the output', '500': '0.5s', '1000': '1s', '1500': '1.5s', '2000': '2s', '2500': '2.5s', '3000': '3s', '3500': '3.5s', '4000': '4s', '4500': '4.5s', '5000': '5s', '10000': '10s', '20000': '20s', '30000': '30s', '40000': '40s', '50000': '50s', '60000': '60s', '90000': '90s', '120000': '120s', '240000': '240s', '300000': '300s', '600000': '600s', '900000': '900s', '1800000': '1800s', '3600000': '3600s']
+private @Field final Map PREFFLASHTIME = ['500': '500ms', '750': '750ms', '1000': '1000ms', '1500': '1500ms', '2000': '2000ms', '2500': '2500ms', '3000': '3000ms', '4000': '4000ms', '5000': '5000ms']
+private @Field final Map PREFFLASHTIMEOUT = ['0': 'never', '1': '1m', '2': '2m', '3': '3m', '4': '4m', '5': '5m', '10': '10m', '15': '15m', '30': '30m', '60': '60m', '90': '90m', '120': '120m', '180': '180m']
+private @Field final Map PREFLOGGING = ['0': 'Device Event Logging', '1': 'Driver Informational Logging', '2': 'Driver Warning Logging', '3': 'Driver Error Logging', '4': 'Driver Debug Logging', '5': 'Driver Trace Logging' ]
+
+void installed() {
+    logTrace('installed called', true)
+    resetPreferencesToDefault()
+}
+
+void resetPreferencesToDefault() {
+    logsOn()
+    logTrace('resetPreferencesToDefault called')
+    settings.keySet().each { String setting ->
+        device.removeSetting(setting)
+    }
+    device.updateSetting('deviceConfigDefaultPowerOnBehaviour', PREFPREVIOUS)
+    logInfo("deviceConfigDefaultPowerOnBehaviour setting is: ${PREFPOWERON[deviceConfigDefaultPowerOnBehaviour]}")
+    device.updateSetting('hubStartupDefaultCommand', [value: 'refresh', type: 'enum'])
+    logInfo("hubStartupDefaultCommand setting is: ${PREFHUBRESTART[hubStartupDefaultCommand]}")
+    device.updateSetting('flashTime', [value: '750', type: 'enum'])
+    logInfo("flashTime setting is: ${PREFFLASHTIME[flashTime]}")
+    device.updateSetting('flashTimeout', PREF10)
+    logInfo("flashTimeout setting is: ${PREFFLASHTIMEOUT[flashTimeout]}")
+    device.updateSetting('automaticOffTime', PREFNONE)
+    logInfo("automaticOffTime setting is: ${PREFAUTOMATICOFFTIME[automaticOffTime]}")
+    logInfo('logging level is: Driver Trace Logging')
+    logInfo("logging level will reduce to Driver Error Logging after ${LOGSOFF} seconds")
+}
+
+void uninstalled() {
+    logTrace('uninstalled called')
+    clearAll()
+}
+
+void initialize() {
+    logTrace('initialize called')
+    String startupDefaultCommand = hubStartupDefaultCommand ?: 'refresh'
+    switch (startupDefaultCommand) {
+        case 'off':
+            off()
+            break
+        case 'on':
+            on()
+            break
+        case 'refresh':
+            refresh()
+            break
+        default:
+            break
+    }
+}
+
+void updated() {
+    logTrace('updated called')
+    logTrace("settings: ${settings}")
+    logInfo("deviceConfigDefaultPowerOnBehaviour setting is: ${PREFPOWERON[deviceConfigDefaultPowerOnBehaviour ?: 'previous']}", true)
+    logInfo("hubStartupDefaultCommand setting is: ${PREFHUBRESTART[hubStartupDefaultCommand ?: 'refresh']}", true)
+    logInfo("flashTime setting is: ${PREFFLASHTIME[flashTime ?: '750']}", true)
+    logInfo("flashTimeout setting is: ${PREFFLASHTIMEOUT[flashTimeout ?: '10']}", true)
+    logInfo("automaticOffTime setting is: ${PREFAUTOMATICOFFTIME[automaticOffTime ?: 'none']}", true)
+    logInfo("logging level is: ${PREFLOGGING[loggingOption]}", true)
+    clearAll()
+    if (logMatch('debug')) {
+        logInfo("logging level will reduce to Driver Error Logging after ${LOGSOFF} seconds", true)
+        runIn(LOGSOFF, logsOff)
+    }
+    if (checkPreferences()) {
+        logInfo('Device Configuration Options have been changed, will now configure the device!', true)
+        configure()
+    }
+}
+
+void logsOff() {
+    logTrace('logsOff called')
+    if (DEBUG) {
+        logDebug('DEBUG field variable is set, not disabling logging automatically!', true)
+    }
+    else {
+        logInfo('automatically reducing logging level to Driver Error Logging', true)
+        device.updateSetting('loggingOption', [value: '3', type: 'enum'])
+    }
+}
+
+void configure() {
+    logTrace('configure called')
+    parent?.componentConfigure(this.device)
+}
+
+void refresh() {
+    logTrace('refresh called')
+    parent?.componentRefresh(this.device)
+}
+
+void onWithAutomaticOff(BigDecimal automaticOffTime) {
+    logTrace("onWithAutomaticOff called automaticOffTime: ${automaticOffTime}")
+    flashStop(false)
+    Integer onTime = (automaticOffTime.toInteger() / 100).toInteger()
+    logDebug("onTime: ${onTime}")
+    if (onTime != 65535) {
+        parent?.componentOn(this.device, onTime)
+    }
+    else {
+        parent?.componentOn(this.device)
+    }
+    state['action'] = 'digitalon'
+}
+
+void on() {
+    logTrace('on called')
+    flashStop(false)
+    logDebug("automatic off time is ${automaticOffTime ?: 'none'}")
+    Integer onTime = automaticOffTime ? automaticOffTime == 'none' ? 65535 : (automaticOffTime.toInteger() / 100).toInteger() : 65535
+    logDebug("onTime: ${onTime}")
+    if (onTime != 65535) {
+        parent?.componentOn(this.device, onTime)
+    }
+    else {
+        parent?.componentOn(this.device)
+    }
+    state['action'] = 'digitalon'
+}
+
+void off() {
+    logTrace('off called')
+    flashStop(false)
+    parent?.componentOff(this.device)
+    state['action'] = 'digitaloff'
+}
+
+void flash(BigDecimal rate = null) {
+    logTrace("flash called rate: ${rate ?: 'no rate specified'}")
+    if (state['flashing']) {
+        logDebug('state["flashing"] is true, stopping flasher')
+        state['flashing'] = false
+        flashStop()
+    }
+    else {
+        logDebug('state["flashing"] is false, starting flasher')
+        state['flashing'] = true
+        String currentState = device.currentValue('switch')
+        logDebug("device state is currently: ${currentState}")
+        state['flashPrevious'] = currentState == 'on' ?: false
+        Integer flashRate = rate
+        if (!flashRate) {
+            logDebug("no rate specified, using flashTime: ${flashTime ?: '750'}")
+            flashRate = flashTime ? flashTime.toInteger() : 750
+        }
+        logDebug("flashRate: ${flashRate}")
+        if (flashRate > 5000 || flashRate < 500) {
+            flashRate = flashRate > 5000 ? 5000 : flashRate < 500 ? 500 : flashRate
+            logWarn('flashRate outside safe range (500 - 5000), resetting to safe value!')
+        }
+        runInMillis(flashRate, flasher, [data: ['on': !(state['flashPrevious']), 'rate': flashRate]])
+        logDebug("flashTimeout: ${flashTimeout ?: '10'}")
+        Integer flashEnd = (flashTimeout ? flashTimeout.toInteger() : 10) * 60 * 1000
+        logDebug("flashEnd: ${flashEnd}")
+        if (flashEnd > 0) {
+            logDebug('setting flashing timeout')
+            runInMillis(flashEnd, flashStop)
+        }
+        else {
+            logDebug('no timeout requested')
+        }
+    }
+}
+
+void flasher(Map data) {
+    logTrace("flasher called data: ${data}")
+    if (state['flashing']) {
+        String action = 'digitalon'
+        if (data.on) {
+            logDebug('turning on')
+            parent?.componentOn(this.device)
+        }
+        else {
+            logDebug('turning off')
+            parent?.componentOff(this.device)
+            action = 'digitaloff'
+        }
+        state['action'] = action
+        runInMillis(data.rate, flasher, [data: ['on': !(data.on), 'rate': data.rate]])
+    }
+    else {
+        logDebug('state["flashing"] is false, skipping!')
+    }
+}
+
+void flashStop(Boolean reinstate = true) {
+    logTrace('flashStop called')
+    state['flashing'] = false
+    unschedule('flasher')
+    unschedule('flashStop')
+    if (reinstate) {
+        logDebug("reinstate is true, reinstating device previous state: ${state['flashPrevious'] ? 'on' : 'off'}")
+        String action = 'digitalon'
+        if (state['flashPrevious']) {
+            logDebug('reinstate device to on')
+            parent?.componentOn(this.device)
+        }
+        else {
+            logDebug('reinstate device to off')
+            parent?.componentOff(this.device)
+            action = 'digitaloff'
+        }
+        state['action'] = action
+    }
+}
+
+void parse(String description) {
+    logTrace('parse called')
+    logWarn("parse(String description) not implemented description: ${description}")
+}
+
+void parse(Map event) {
+    logTrace('parse called')
+    if (event) {
+        logDebug("got event: ${event}")
+        if (event.name == 'switch') {
+            String descriptionText = "${device.displayName} was turned ${event.value}"
+            String currentValue = device.currentValue('switch')
+            if (event.value == currentValue) {
+                descriptionText = "${device.displayName} is ${event.value}"
+            }
+            String type = 'physical'
+            String action = state['action'] ?: 'standby'
+            if (action == 'digitalon' || action == 'digitaloff') {
+                logDebug("action is ${action}")
+                type = 'digital'
+                state['action'] = 'standby'
+                logDebug('action set to standby')
+            }
+            logEvent(descriptionText)
+            sendEvent(processEvent([name: 'switch', value: event.value, type: type, descriptionText: descriptionText]))
+        }
+        else if (event.name == 'startupBehaviour') {
+            Map<String, String> startUpOnOff = ['00': 'off', '01': 'on', '02': 'opposite', '03': 'previous', 'FF': 'previous']
+            String startupBehaviour = PREFPOWERON[startUpOnOff[event.value]] ?: "${event.value} (unknown}"
+            logDebug("deviceConfigDefaultPowerOnBehaviour is currently set to: ${PREFPOWERON[deviceConfigDefaultPowerOnBehaviour ?: 'previous']} and device reports it is set to: ${startupBehaviour}")
+        }
+        else {
+            logWarn('unexpected event type!')
+        }
+    }
+    else {
+        logWarn('empty event!')
+    }
+}
+
+String lookupData(String dataType, String dataName) {
+    logTrace("lookupData called dataType: ${dataType} dataName: ${dataName}")
+    String lookupDataValue = null
+    switch (dataType) {
+        case 'state':
+            lookupDataValue = state[dataName]
+            break
+        case 'setting':
+            lookupDataValue = settings[dataName]
+            break
+        case 'data':
+            lookupDataValue = getDataValue(dataName)
+            break
+        default:
+            break
+    }
+    logDebug("${dataType} ${dataName}: ${lookupDataValue}")
+    return lookupDataValue
+}
+
+private Map processEvent(Map event) {
+    logTrace("processEvent called data: ${event}")
+    return createEvent(event)
+}
+
+private Boolean logMatch(String logLevel) {
+    Map<String, String> logLevels = ['event': '0', 'info': '1', 'warn': '2', 'error': '3', 'debug': '4', 'trace': '5' ]
+    return loggingOption ? loggingOption.toInteger() >= logLevels[logLevel].toInteger() : true
+}
+
+private String logTrace(String msg, Boolean override = false) {
+    if (logMatch('trace') || override) {
+        log.trace(logMsg(msg))
+    }
+}
+
+private String logDebug(String msg, Boolean override = false) {
+    if (logMatch('debug') || override) {
+        log.debug(logMsg(msg))
+    }
+}
+
+private String logError(String msg, Boolean override = false) {
+    if (logMatch('error') || override) {
+        log.error(logMsg(msg))
+    }
+}
+
+private String logWarn(String msg, Boolean override = false) {
+    if (logMatch('warn') || override) {
+        log.warn(logMsg(msg))
+    }
+}
+
+private String logInfo(String msg, Boolean override = false) {
+    if (logMatch('info') || override) {
+        log.info(logMsg(msg))
+    }
+}
+
+private String logEvent(String msg, Boolean override = false) {
+    if (logMatch('event') || override) {
+        log.info(logMsg(msg))
+    }
+}
+
+private String logMsg(String msg) {
+    String log = "candeo logging for ${CANDEO} -- "
+    log += msg
+    return log
+}
+
+private void logsOn() {
+    logTrace('logsOn called', true)
+    device.updateSetting('loggingOption', PREF5)
+    runIn(LOGSOFF, logsOff)
+}
+
+private void clearAll() {
+    logTrace('clearAll called')
+    state.clear()
+    atomicState.clear()
+    unschedule()
+}
+
+private Boolean checkPreference(String preference) {
+    logTrace("checkPreference called preference: ${preference}")
+    String oldPreference = preference + '_OLD'
+    String newPreferenceValue = settings.containsKey(preference) ? settings[preference].toString() : 'unknown'
+    logDebug("newPreferenceValue: ${newPreferenceValue}")
+    String oldPreferenceValue = settings.containsKey(oldPreference) ? settings[oldPreference].toString() : 'unknown'
+    logDebug("oldPreferenceValue: ${oldPreferenceValue}")
+    if (oldPreferenceValue != newPreferenceValue) {
+        device.updateSetting(oldPreference, newPreferenceValue)
+        return true
+    }
+    return false
+}
+
+private Boolean checkPreferences() {
+    logTrace('checkPreferences called')
+    Set deviceConfig = settings.keySet().findAll { String preference ->
+        preference.startsWith('deviceConfig') && preference.indexOf('_OLD') == -1 && checkPreference(preference)
+    }
+    return (deviceConfig.size() > 0)
+}
